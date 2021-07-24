67b20e391a27fb6c854466e87a0f1c40df71e697
yuksi-root <yukselcansimsek@gmail.com>
2021-07-24 22:27:47 +0300

    add context_extension for dynamic values

---
+++ lib/core/extension/context_extension.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+extension ContextExtension on BuildContext {
+  double dynamicWidth(double val) => MediaQuery.of(this).size.width * val;
+  double dynamicHeight(double val) => MediaQuery.of(this).size.height * val;
+}
