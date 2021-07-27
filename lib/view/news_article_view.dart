a83d2147b0ac6e116d415cfa71d196f5dfa8ef05
yuksi-root <yukselcansimsek@gmail.com>
2021-07-27 18:59:55 +0300

    add  state management to views

--- lib/view/news_article_view.dart
+++ lib/view/news_article_view.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_news_app_with_api/components/news_grid.dart';
-import 'package:flutter_news_app_with_api/view/tabbar_view.dart';
-import 'package:flutter_news_app_with_api/core/constants/api_constants.dart';
-import 'package:flutter_news_app_with_api/core/preferences/shared_manager.dart';
 import 'package:flutter_news_app_with_api/view_models/news_article_list_view_model.dart';
 import 'package:provider/provider.dart';
 import 'dart:developer' as developer;
@@ -15,31 +12,39 @@ class NewsView extends StatefulWidget {
 }
 
 class _NewsViewState extends State<NewsView> {
-  bool isCached = false;
   @override
   void initState() {
     super.initState();
-    Provider.of<NewsArticleListViewModel>(context, listen: false)
-        .topHeadlines();
   }
 
   @override
   Widget build(BuildContext context) {
-    var listViewModel = Provider.of<NewsArticleListViewModel>(context);
-    //developer.log('article_length' + listViewModel.articles.length.toString());
-    /*  listViewModel.articles.forEach((element) {
-      developer.log(element.title);
-    }); */
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Expanded(
-            child: NewsGrid(articles: listViewModel.articles),
-          ),
-        ],
-      ),
+        body: context.watch<NewsArticleListViewModel>().state ==
+                ArticleState.searching
+            ? buildLoadingWidget()
+            : context.watch<NewsArticleListViewModel>().state ==
+                    ArticleState.error
+                ? buildErrorWidget()
+                : buildColumnExpandedNewsGrid(context));
+  }
+
+  Column buildColumnExpandedNewsGrid(BuildContext context) {
+    final listArticles = context.read<NewsArticleListViewModel>().articles;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Expanded(
+          child: NewsGrid(articles: listArticles),
+        ),
+      ],
     );
   }
+
+  Center buildErrorWidget() => Center(child: Text('Something went wrong!'));
+  Center buildLoadingWidget() => Center(
+          child: CircularProgressIndicator(
+        color: Colors.green,
+      ));
 }
