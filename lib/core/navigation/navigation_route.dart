bfdffd602f57bdb16562a39509331383e16e93c9
yuksi-root <yukselcansimsek@gmail.com>
2021-08-01 18:54:35 +0300

    pass data to NewsDetailsView fixed

--- lib/core/navigation/navigation_route.dart
+++ lib/core/navigation/navigation_route.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_news_app_with_api/core/constants/navigation_constants.dart';
 import 'package:flutter_news_app_with_api/view/news_article_details_view.dart';
-import 'package:flutter_news_app_with_api/view/news_article_view.dart';
+import 'package:flutter_news_app_with_api/view/tabbar_view.dart';
+import 'package:flutter_news_app_with_api/view_models/news_article_view_model.dart';
 
 class NavigationRoute {
   static final NavigationRoute _instance = NavigationRoute._init();
@@ -9,21 +10,27 @@ class NavigationRoute {
 
   NavigationRoute._init();
 
-  Route<dynamic> generateRoute(RouteSettings settings) {
-    print('Generating route: ${settings.name}');
-    switch (settings.name) {
-      case NavigationConstants.HOME_VIEW:
-        print('aaaaaaaa');
-        return pageNavigate(NewsView());
+  Route<dynamic> generateRoute(RouteSettings? settings) {
+    print('Generating route: ${settings?.name}');
 
+    switch (settings?.name) {
+      case NavigationConstants.HOME_VIEW:
+        print("newsTabbarView");
+        return pageNavigate(NewsTabbarView());
       case NavigationConstants.NEWS_DETAILS_WIEW:
-        return pageNavigate(NewsDetailsView(article: null));
+        print("newsDetailView");
+        if (settings?.arguments is NewsArticleViewModel) {
+          return pageNavigate(NewsDetailsView(
+            article: settings!.arguments as NewsArticleViewModel,
+          ));
+        }
+        throw ("Article model is not Found");
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
             appBar: AppBar(),
             body: Center(
-              child: Text('Route NOT FOUND!'),
+              child: Text('Route is  not Found'),
             ),
           ),
         );
