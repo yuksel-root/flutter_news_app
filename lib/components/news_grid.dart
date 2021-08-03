95cbf379cd4bcf6b5a5644a87d6764f34e55e9cb
yuksi-root <yukselcansimsek@gmail.com>
2021-08-03 06:58:48 +0300

    update newsGrid component

--- lib/components/news_grid.dart
+++ lib/components/news_grid.dart
@@ -4,6 +4,9 @@ import 'package:flutter_news_app_with_api/core/constants/navigation_constants.da
 import 'package:flutter_news_app_with_api/core/navigation/navigation_service.dart';
 import 'package:flutter_news_app_with_api/view_models/news_article_view_model.dart';
 
+//TODO add article.category
+//TODO add dynamic padding size
+
 class NewsGrid extends StatelessWidget {
   final NavigationService navigation = NavigationService.instance;
   final List<NewsArticleViewModel> articles;
@@ -25,21 +28,85 @@ class NewsGrid extends StatelessWidget {
             _showNewsArticleDetails(context, article);
           },
           child: GridTile(
-              child: Container(
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                child: CircleImages(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: Stack(children: [
+                CircleImages(
                   imageUrl: article.imageUrl,
                   imageRadius: 10,
                 ),
-              ),
-              footer: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 13),
-                child: Text(article.title,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis),
-              )),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                    colors: [
+                                      Color(0xffbdc3c7),
+                                      Color(0xccbdc3c7)
+                                    ],
+                                    tileMode: TileMode.clamp)),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8.0, vertical: 0),
+                              child: Text("Teknoloji",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .subtitle2
+                                      ?.copyWith(color: Colors.white),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                    colors: [
+                                      Color(0xff2c3e50),
+                                      Color(0xcc2c3e50)
+                                    ],
+                                    tileMode: TileMode.clamp)),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8.0, vertical: 0),
+                              child: Text(article.source,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .subtitle2
+                                      ?.copyWith(color: Colors.white),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      child: Text(article.title,
+                          style: Theme.of(context)
+                              .textTheme
+                              .subtitle1
+                              ?.copyWith(color: Colors.white),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis),
+                    ),
+                  ],
+                ),
+              ]),
+            ),
+          ),
         );
       },
       itemCount: articles.length,
