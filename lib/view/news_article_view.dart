58f79630995fa2286036410e76e77408a3eb9013
yuksi-root <yukselcansimsek@gmail.com>
2021-08-03 07:01:57 +0300

    update news_article_view  to StatelessWidget

--- lib/view/news_article_view.dart
+++ lib/view/news_article_view.dart
@@ -2,24 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_news_app_with_api/components/news_grid.dart';
 import 'package:flutter_news_app_with_api/view_models/news_article_list_view_model.dart';
 import 'package:provider/provider.dart';
-import 'dart:developer' as developer;
 
-class NewsView extends StatefulWidget {
+class NewsView extends StatelessWidget {
   const NewsView({Key? key}) : super(key: key);
-
-  @override
-  _NewsViewState createState() => _NewsViewState();
-}
-
-class _NewsViewState extends State<NewsView> {
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color(0xe61c0f45),
         body: context.watch<NewsArticleListViewModel>().state ==
                 ArticleState.searching
             ? buildLoadingWidget()
