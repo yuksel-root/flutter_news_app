c865ccfcf7b2aa28b76f33761079cf97ac70dffb
yuksi-root <yukselcansimsek@gmail.com>
2021-08-05 16:37:50 +0300

    add country and categories constants

---
+++ lib/core/constants/categories_constants.dart
@@ -0,0 +1,28 @@
+class CategoriesConstants {
+  static const List<dynamic> listCategory = <dynamic>[
+    {
+      'categoryName': 'General',
+      'categoryCode': 'general',
+    },
+    {
+      'categoryName': 'Business',
+      'categoryCode': 'business',
+    },
+    {
+      'categoryName': 'Technology',
+      'categoryCode': 'technology',
+    },
+    {
+      'categoryName': 'Sports',
+      'categoryCode': 'sports',
+    },
+    {
+      'categoryName': 'Health',
+      'categoryCode': 'health',
+    },
+    {
+      'categoryName': 'Entertainment',
+      'categoryCode': 'entertainment',
+    },
+  ];
+}
---
+++ lib/core/constants/country_constants.dart
@@ -0,0 +1,61 @@
+class CountryConstants {
+  static const imagePath = "assets/icons/flags/png/w100px/";
+
+  static const List<dynamic> listCountry = <dynamic>[
+    {
+      'countryName': 'Turkey',
+      'countryCode': 'tr',
+      'imagePath': imagePath + 'tr.png'
+    },
+    {
+      'countryName': 'United States of America',
+      'countryCode': 'us',
+      'imagePath': imagePath + 'us.png'
+    },
+    {
+      'countryName': 'Germany',
+      'countryCode': 'de',
+      'imagePath': imagePath + 'de.png'
+    },
+    {
+      'countryName': 'Russia',
+      'countryCode': 'ru',
+      'imagePath': imagePath + 'ru.png'
+    },
+    {
+      'countryName': 'France',
+      'countryCode': 'fr',
+      'imagePath': imagePath + 'fr.png'
+    },
+    {
+      'countryName': 'Italy',
+      'countryCode': 'it',
+      'imagePath': imagePath + 'it.png'
+    },
+    {
+      'countryName': 'Argentina',
+      'countryCode': 'ar',
+      'imagePath': imagePath + 'ar.png'
+    },
+    {
+      'countryName': 'Australia',
+      'countryCode': 'au',
+      'imagePath': imagePath + 'au.png'
+    },
+    {
+      'countryName': 'India',
+      'countryCode': 'in',
+      'imagePath': imagePath + 'in.png'
+    },
+    {
+      'countryName': 'Korea',
+      'countryCode': 'kr',
+      'imagePath': imagePath + 'kr.png'
+    },
+    {
+      'countryName': 'China',
+      'countryCode': 'ch',
+      'imagePath': imagePath + 'ch.png'
+    },
+  ];
+}
