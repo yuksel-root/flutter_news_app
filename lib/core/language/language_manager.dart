a51cc5012625e55c39bf01e7d28b7859db5c9d56
yuksi-root <yukselcansimsek@gmail.com>
2021-08-06 23:59:48 +0300

    add localization manager

---
+++ lib/core/language/language_manager.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LanguageManager {
+  static LanguageManager? _instance;
+  static LanguageManager get instance {
+    if (_instance == null) _instance = LanguageManager._init();
+    return _instance!;
+  }
+
+  LanguageManager._init();
+
+  final trLocale = Locale("tr", "TR");
+  final enLocale = Locale("en", "US");
+
+  List<Locale> get supportedLocales => [enLocale, trLocale];
+}
---
+++ lib/core/language/locale_keys.g.dart
@@ -0,0 +1,29 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const categories_tab1 = 'categories.tab1';
+  static const categories_tab2 = 'categories.tab2';
+  static const categories_tab3 = 'categories.tab3';
+  static const categories_tab4 = 'categories.tab4';
+  static const categories_tab5 = 'categories.tab5';
+  static const categories_tab6 = 'categories.tab6';
+  static const categories = 'categories';
+  static const countries_country1 = 'countries.country1';
+  static const countries_country2 = 'countries.country2';
+  static const countries_country3 = 'countries.country3';
+  static const countries_country4 = 'countries.country4';
+  static const countries_country5 = 'countries.country5';
+  static const countries_country6 = 'countries.country6';
+  static const countries_country7 = 'countries.country7';
+  static const countries_country8 = 'countries.country8';
+  static const countries_country9 = 'countries.country9';
+  static const countries_country10 = 'countries.country10';
+  static const countries_country11 = 'countries.country11';
+  static const countries = 'countries';
+  static const bottomNav_tab1 = 'bottomNav.tab1';
+  static const bottomNav_tab2 = 'bottomNav.tab2';
+  static const bottomNav = 'bottomNav';
+  static const newsDetails_goArticleButton = 'newsDetails.goArticleButton';
+  static const newsDetails = 'newsDetails';
+
+}
